5692b2cbb81ac6ae2390f8b97ee26eff1f2eca07
wesam abu shaqra <80597414+wesamalaliwa@users.noreply.github.com>
2022-11-28 21:42:56 +0300

    taasskk

--- 27-NOV/Program.cs
+++ 27-NOV/Program.cs
@@ -23,24 +23,23 @@ namespace _27_NOV
             Email = Email1;
             id = id1;
             phone = phone1;
+
+            if (age == 18 && age == 60)
+            {
+                Console.WriteLine($"you are {age} years old");
+            }
+            else
+            {
+                Console.WriteLine("your age out of the range");
+            };
         }
       
         public static void print(int age1, string gender1, string name1, string Email1, int id1, int phone1)
         {
-            Console.WriteLine($" id: {id1}, gender {gender1}, the name is: {name1}, Email:{Email1},age:{age1}");
+            Console.WriteLine($" id: {id1}, gender: {gender1}, the name is: {name1}, Email:{Email1},age:{age1}");
          }
 
-        public static void agee(int age)
- 
-        {
-            if (age == 18 ||age < 60)
-            {
-                Console.WriteLine($"you are {age} years old");
-            }
-            else {
-                Console.WriteLine("your age out of the range");
-                    }
-        }
+       
         
         public static void phonestarting(string phone) 
         {
@@ -55,10 +54,12 @@ namespace _27_NOV
             }
         }
 
+
             static void Main(string[] args)
         {
-            identity myage = new identity(25,"male","wesam","abushaqrawesam@gmail.com",34,0780290566);
-            identity.agee(25);
+            identity myage = new identity(1,"male","wesam","abushaqrawesam@gmail.com",34,0780290566);
+            identity.print(25, "male", "wesam", "abushaqrawesam@gmail.com", 34, 0780290566);
+          
             identity.phonestarting("0780289");
         }
     }
---
+++ 28-NOV/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _28_NOV
+{
+    internal class Cars
+    {
+
+        public string make { set; get; }
+        public int year { set; get; }
+        public string type { set; get; }
+        public int price { set; get; }
+        public string model { set; get; }
+        public int paletNo { set; get; }
+        public string color { set; get; }
+
+        public Cars(string make1, int year1, string type1, int price1, string model1, int paletNo1, string color1)
+        {
+            this.make = make1;
+            this.year = year1;
+            this.type = type1;
+            this.price = price1;
+            this.model = model1;
+            this.paletNo = paletNo1;
+            this.color = color1;
+
+        }
+        public void start()
+        {
+            Console.WriteLine("car is start");
+        }
+        public void stop()
+        {
+            Console.WriteLine("car is stop");
+        }
+        public void print()
+        {
+            Console.WriteLine($"the maker is:{make}, year of made:{year}, car type is{type}, car price:{price}, car model:{model},the palet number is:{paletNo}, car color is:{color}");
+        }
+    }
+    class toyota : Cars
+    {
+        public string Shape { get; set; }
+        public int Mirror { get; set; }
+        public toyota(string make1, int year1, string type1, int price1, string model1, int paletNo1, string color1) : base(make1, year1, type1, price1, model1, paletNo1, color1)
+        {
+
+        }
+
+        public toyota(string make1, int year1, string type1, int price1, string model1, int paletNo1, string color1, string shape, int mirror) : base(make1, year1, type1, price1, model1, paletNo1, color1)
+        {
+            this.Shape = shape;
+            Mirror = mirror;
+        }
+        public void print1()
+        {
+            Console.WriteLine($"the shape is:{Shape}, number of mirror:{Mirror}");
+        }
+        public void distance(int l)
+        {
+            int dis = 20 * l;
+            Console.WriteLine($"the distance = {dis}");
+        }
+    }
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter your car maker");
+            string make = Console.ReadLine();
+
+            Console.WriteLine("enter your car year");
+            int year = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("enter your car type");
+            string type = Console.ReadLine();
+
+            Console.WriteLine("enter your car price");
+            int price = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("enter your car model");
+            string model = Console.ReadLine();
+
+            Console.WriteLine("enter your car palet number");
+            int paletNum = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("enter your car color");
+            string color = Console.ReadLine();
+
+
+            Cars myCar = new Cars(make, year, type, price, model, paletNum, color);
+
+            myCar.print();
+            myCar.start();
+            myCar.stop();
+
+            Console.WriteLine("enter your car shape");
+            string shape = Console.ReadLine();
+
+            Console.WriteLine("enter your number of mirror");
+            int mirror = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("enter  number of litter");
+            int litter = Convert.ToInt32(Console.ReadLine());
+
+            toyota car = new toyota(make, year, type, price, model, paletNum, color, shape, mirror);
+
+            car.distance(litter);
+            car.print();
+            car.print1();
+
+        }
+    }
+}
